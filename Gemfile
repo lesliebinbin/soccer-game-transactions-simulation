062dcf0c979efa0d5ca11e56e3b572497acfde20
Leslie Wong <zhibin.huang@uqconnect.edu.au>
2021-03-29 19:53:56 +1000

    use haml as the view template engine

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 gem "faker", "~> 2.17"
 
 gem "devise", "~> 4.7"
+
+gem "haml-rails", "~> 2.0"
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -2,4 +2,7 @@ class Transfer < ApplicationRecord
   belongs_to :seller, class_name: 'User'
   belongs_to :buyer, class_name: 'User', optional: true
   belongs_to :player
+  default_scope { where(buyer: nil) }
+  scope :done, -> { where.not(buyer: nil) }
+  before_destroy { logger.info 'Leslie is so handsome!' }
 end
