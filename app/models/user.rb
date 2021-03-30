2c520a3809615abc45259ab199c58a9c03848d8a
Leslie Wong <zhibin.huang@uqconnect.edu.au>
2021-03-30 13:49:15 +1000

    adding jwt authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   has_one :team
   has_many :players, through: :team
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 end
