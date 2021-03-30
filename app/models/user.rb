abbb29e3e18bcd1f8c394196286d051b3fda0a0e
Leslie Wong <zhibin.huang@uqconnect.edu.au>
2021-03-30 16:15:45 +1000

    adding hook to create team and players for a new registered non-admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :team
   has_many :players, through: :team
+  after_create :init_team_and_players
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, :confirmable,
+         :jwt_authenticatable,
+         # :confirmable,
          jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  private
+
+  def init_team_and_players
+    return if admin_role?
+
+    ActiveRecord::Base.transaction do
+      team, players = Generators::General.generate(team: 1, player: 20).values_at(:team, :player)
+      create_team(team.first)
+      self.team.players.create(players)
+    end
+  end
 end
